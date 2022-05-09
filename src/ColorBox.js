17dab3a7800a5eef61c22335808857436eb48b85
Jana Abfalterer <abfalterer.jana@gmail.com>
2022-05-09 17:19:20 +0200

    Add input boxes

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,11 +1,18 @@
+// colored box with button to generate random colors
+
 import randomColor from 'randomcolor';
 import { useState } from 'react';
 
 export default function ColorBox() {
   const [color, setColor] = useState(randomColor());
+  const [hue, setHue] = useState('');
+  const [lum, setLum] = useState('');
+  const [height, setHeight] = useState('');
+  const [width, setWidth] = useState('');
   return (
     <>
       <div
+        className="box-style"
         style={{
           display: 'flex',
           justifyContent: 'center',
@@ -15,20 +22,70 @@ export default function ColorBox() {
       >
         <div
           style={{
-            height: '20vh',
-            width: '20vh',
+            height: height ? height + 'px' : '20vh',
+            width: width ? width + 'px' : '20vh',
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
             textAlign: 'center',
-            backgroundColor: color,
+            backgroundColor:
+              hue || lum ? randomColor({ hue: hue, luminosity: lum }) : color,
+            transition: '1.5s',
           }}
         >
           Generated Color: {color}
         </div>
       </div>
+
       <br />
       <button onClick={() => setColor(randomColor())}>Generate</button>
+
+      <br />
+      <br />
+
+      <div className="input">
+        <div>define hue: </div>
+
+        <input
+          value={hue}
+          onChange={(event) => {
+            setHue(event.currentTarget.value);
+          }}
+        />
+
+        <br />
+        <br />
+        <div>prefer light or dark?</div>
+
+        <input
+          value={lum}
+          onChange={(event) => {
+            setLum(event.currentTarget.value);
+          }}
+        />
+      </div>
+
+      <div className="input">
+        <br />
+        <br />
+        <div>define box height (px):</div>
+        <input
+          value={height}
+          onChange={(event) => {
+            setHeight(event.currentTarget.value);
+          }}
+        />
+
+        <br />
+        <br />
+        <div>define box with (px):</div>
+        <input
+          value={width}
+          onChange={(event) => {
+            setWidth(event.currentTarget.value);
+          }}
+        />
+      </div>
     </>
   );
 }
